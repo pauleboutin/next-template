b2d4f0dc0ef4d51326bf269e7332afd58dbb8c7a
Paul Boutin <boutin@gmail.com>
2024-08-04 14:12:16 -0700

    edited Title again

--- components/Title.tsx
+++ components/Title.tsx
@@ -3,5 +3,5 @@
 import { useLekkoConfig } from "@lekko/next-sdk";
 
 export function Title() {
-  return <>I will try controlling this text with Lekko!</>;
+  return <>I will try controlling this text with Lekko</>;
 }
