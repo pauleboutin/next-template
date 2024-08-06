abe2016df91152c5e0572dbcee5bd231552d5423
Paul Boutin <boutin@gmail.com>
2024-08-06 10:49:29 -0700

    added test2.ts lekkos

---
+++ lekko/test2.ts
@@ -0,0 +1,63 @@
+/** Example feature flag, enabled in development environments */
+
+export function getExampleFlag2({ env }: { env: string }): boolean {
+  if (env === "development") {
+    return true;
+  }
+  return false;
+}
+
+/** Example lekko that controls which LLM users interact with */
+export function getExampleModel2({
+  isAdmin,
+  plan,
+}: {
+  isAdmin: boolean;
+  plan: string;
+}): string {
+  if (isAdmin) {
+    return "anthropic-claude3-opus";
+  } else if (plan === "team" || plan === "enterprise") {
+    return "openai-chatgpt4-o";
+  }
+  return "openai-chatgpt3.5-turbo";
+}
+
+/** Example lekko that controls sampling rate based on contextual factors */
+export function getExampleSampleRate2({
+  env,
+  load,
+  msgType,
+  throttle,
+}: {
+  env: string;
+  load: number;
+  msgType: string;
+  throttle: boolean;
+}): number {
+  if (env === "staging") {
+    return 1;
+  } else if (msgType === "critical") {
+    return 1;
+  } else if (load > 0.75 || throttle) {
+    return 0.3;
+  }
+  return 0.75;
+}
+
+/** Example lekko that controls the title based on a variable */
+export function getTitle2({
+  enviro
+}: {
+  enviro: string;
+}): string {
+  if (enviro === "development") {
+    return "DEVELOPMENT";
+  } else if (enviro === "staging") {
+    return "STAGING";
+  } else if (enviro === "production") {
+    return "PRODUCTION";
+  } 
+  return "UNDEFINED";
+  
+}
