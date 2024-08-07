9ef39c134028324ba15df82fc04178f574d34c09
Paul Boutin <boutin@gmail.com>
2024-08-07 01:02:33 -0700

    VERCEL_ENV

--- components/Title.tsx
+++ components/Title.tsx
@@ -4,6 +4,21 @@ import { useLekkoConfig } from "@lekko/next-sdk";
 import { getTitle } from "../lekko/paultest";
 import { useLogger } from 'next-axiom';
 
+interface VercelData {
+  fields: {
+    path: string;
+  };
+  level: string;
+  message: string;
+  vercel: {
+    environment: string;
+    projectId: string;
+    region: string;
+    route: string;
+    source: string;
+    teamName: string;
+  };
+}
 
 // Ensure environment variables are loaded
 if (typeof window === 'undefined') {
@@ -12,11 +27,12 @@ if (typeof window === 'undefined') {
 
 export function Title() {
   const env = process.env.ENV || 'ENV not set';
-  const log = useLogger();
-  //log.debug('ENV environment variable = ', { env});
-  log.debug(`ENV = ${env}`);
+  const vercelEnv = process.env.VERCEL_ENV || 'Vercel environment undefined';
 
-  //log.debug();
+  const log = useLogger();
+  log.debug(`ENV = ${env}`);
+  log.debug(`vercel.environment = ${vercelEnv}`);
+  
 
   let titletext = useLekkoConfig(getTitle, { enviro: env });
   titletext = "This is a " + titletext + " environment";
