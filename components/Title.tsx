f044448b6530e6452297f7fd2f70f551c126ab67
Paul Boutin <boutin@gmail.com>
2024-08-06 23:30:27 -0700

    added axiom log.debug event

--- components/Title.tsx
+++ components/Title.tsx
@@ -2,6 +2,8 @@
 
 import { useLekkoConfig } from "@lekko/next-sdk";
 import { getTitle } from "../lekko/paultest";
+import { useLogger } from 'next-axiom';
+
 
 // Ensure environment variables are loaded
 if (typeof window === 'undefined') {
@@ -10,6 +12,8 @@ if (typeof window === 'undefined') {
 
 export function Title() {
   const env = process.env.ENV || 'ENV not set';
+  const log = useLogger();
+  log.debug('ENV is set to', { env });
   let titletext = useLekkoConfig(getTitle, { enviro: env });
   titletext = "This is a " + titletext + " environment";
   return titletext;
