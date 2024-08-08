9c09a55a69fb2610c3fd5f4cef82df4bef70818b
Paul Boutin <boutin@gmail.com>
2024-08-07 22:49:50 -0700

    I think this works -- it compiles

--- lekko/paultest.ts
+++ lekko/paultest.ts
@@ -1,4 +1,48 @@
 
+/** Example lekko that retuns a complex object */
+export interface Merchant {
+  DeliveryRadius: number;
+  Hours: string;
+  ParkingLocation: string;
+  PrepTime: number;
+  Sources: string[];
+}
+
+export function getMerchant(merchantId: string): Merchant | null {
+  if (merchantId === "burger-central") {
+      return {
+          DeliveryRadius: 15,
+          Hours: "9:00AM - 5:00PM",
+          ParkingLocation: "lot_5",
+          PrepTime: 15,
+          Sources: [
+              "uber_eats",
+              "doordash",
+          ],
+      };
+  } else if (merchantId === "huangshen-home") {
+      return {
+          DeliveryRadius: 15,
+          Hours: "9:00AM - 6:00PM",
+          ParkingLocation: "lot_5",
+          PrepTime: 15,
+          Sources: [
+              "uber_eats",
+              "doordash",
+              "native",
+          ],
+      };
+  }
+  return  {
+    DeliveryRadius: 0,
+    Hours: "None",
+    ParkingLocation: "None",
+    PrepTime: 0,
+    Sources: [
+        "None",
+    ],
+  };
+}
 
 /** Example lekko that controls the title based on a variable */
 export function getTitle({ enviro }: { enviro: string }): string {
@@ -17,30 +61,3 @@ export function getTitle({ enviro }: { enviro: string }): string {
   }
   return "NO_ENVIRONMENT_DECLARED";
 }
-type Merchant = {
-  DeliveryRadius: number;
-  Hours: string;
-  ParkingLocation: string;
-  PrepTime: number;
-  Sources: string[];
-};
-
-export function getMerchant({merchantId}: {merchantId: string}): Merchant {
-  if (merchantId === "burger-central"){
-    return {
-      DeliveryRadius:  15,
-      Hours:           "9:00AM - 5:00PM",
-      ParkingLocation: "lot_5",
-      PrepTime:        15,
-      Sources: ["uber_eats", "doordash"]
-    };
-  }
-  /* else /*if (merchantId === "huangshen-home")*/ 
-    return {
-      DeliveryRadius:  15,
-      Hours:           "9:00AM - 6:00PM",
-      ParkingLocation: "lot_5",
-      PrepTime:        15,
-      Sources: ["uber_eats", "doordash", "native"]
-    };
-  }
